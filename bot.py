8bbd085295391dcea0c7a874344a14b3ae191017
gauravbhalar95 <141895800+gauravbhalar95@users.noreply.github.com>
2025-02-02 23:31:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,8 +6,8 @@ from handlers.youtube_handler import process_youtube
 from handlers.instagram_handler import process_instagram
 from utils.thumb_generator import generate_thumbnail
 from config import API_TOKEN, PORT, WEBHOOK_URL
-from handlers.xvideo_haldler import extract_video_id
-from handlers.comman
+from handlers.xvideo_handler import extract_video_id
+from handlers.common_handler import process_adult
 
 # Initialize bot
 bot = telebot.TeleBot(API_TOKEN, parse_mode='HTML')
