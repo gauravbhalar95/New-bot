e14d072d4e49574f62fc050c7c2835cf08c5c443
gauravbhalar95 <141895800+gauravbhalar95@users.noreply.github.com>
2025-02-04 08:45:48 +0530

    Update common_handler.py

--- handlers/common_handler.py
+++ handlers/common_handler.py
@@ -1,8 +1,7 @@
 import os
 import re
-import moviepy
 import cloudscraper
-from moviepy.editor import VideoFileClip
+from moviepy import VideoFileClip
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
