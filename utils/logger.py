5ef75f19add749b9f58f44a0da1e229c2d4a0f62
gauravbhalar95 <141895800+gauravbhalar95@users.noreply.github.com>
2025-02-14 22:48:13 +0530

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,5 +1,6 @@
 import logging
 
+def setup_logging():
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
@@ -9,4 +10,4 @@ logging.basicConfig(
     ]
 )
 
-logger = logging.getLogger(__name__)
+    return logger = logging.getLogger(__name__)
