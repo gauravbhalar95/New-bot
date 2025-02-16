1790081ed7fdfc244504db374679dec7d0bfe77a
gauravbhalar95 <141895800+gauravbhalar95@users.noreply.github.com>
2025-02-16 18:05:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -97,18 +97,18 @@ def handle_message(message):
 
 threading.Thread(target=worker, daemon=True).start()
 
-app = Flask(__name__)
-
-@app.route('/' + API_TOKEN, methods=['POST'])
-def webhook():
-    bot.process_new_updates([types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "OK", 200
-
-@app.route('/')
-def set_webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url=WEBHOOK_URL + '/' + API_TOKEN, timeout=60)
-    return "Webhook set", 200
-
-if __name__ == '__main__':
+app = Flask(__name__)  
+  
+@app.route('/' + API_TOKEN, methods=['POST'])  
+def webhook():  
+    bot.process_new_updates([types.Update.de_json(request.stream.read().decode("utf-8"))])  
+    return "OK", 200  
+  
+@app.route('/')  
+def set_webhook():  
+    bot.remove_webhook()  
+    bot.set_webhook(url=WEBHOOK_URL + '/' + API_TOKEN, timeout=60)  
+    return "Webhook set", 200  
+  
+if __name__ == '__main__':  
     app.run(host='0.0.0.0', port=PORT)
