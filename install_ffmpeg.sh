6ae7e4194a96d023000bfb8431e36e41ee219166
gauravbhalar95 <141895800+gauravbhalar95@users.noreply.github.com>
2025-02-19 22:25:54 +0530

    Update install_ffmpeg.sh

--- install_ffmpeg.sh
+++ install_ffmpeg.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo "Updating package lists and installing ffmpeg..."
+
+# Update package list and install FFmpeg
 apt-get update && apt-get install -y ffmpeg
-echo "ffmpeg installed successfully."
+
