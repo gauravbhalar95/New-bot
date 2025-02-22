e34396ea762863eb773c389c3a9e78b4f5827767
gauravbhalar95 <141895800+gauravbhalar95@users.noreply.github.com>
2025-02-22 22:56:02 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,22 +1,20 @@
-from flask import Flask, request
-from telebot import types
-from config import API_TOKEN, WEBHOOK_URL, PORT
-from bot import bot
-
-app = Flask(__name__)
-
-@app.route('/' + API_TOKEN, methods=['POST'])
-def webhook():
-    json_str = request.stream.read().decode("utf-8")
-    update = types.Update.de_json(json_str)
-    bot.process_new_updates([update])
-    return "OK", 200
-
-@app.route('/')
-def set_webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url=f"{WEBHOOK_URL}/{API_TOKEN}", timeout=60)
-    return "Webhook set", 200
-
-if __name__ == '__main__':
+from flask import Flask, request  
+from telebot import types  
+from config import API_TOKEN, WEBHOOK_URL, PORT  
+from bot import bot  
+  
+app = Flask(__name__)  
+  
+@app.route('/' + API_TOKEN, methods=['POST'])  
+def webhook():  
+    bot.process_new_updates([types.Update.de_json(request.stream.read().decode("utf-8"))])  
+    return "OK", 200  
+  
+@app.route('/')  
+def set_webhook():  
+    bot.remove_webhook()  
+    bot.set_webhook(url=WEBHOOK_URL + '/' + API_TOKEN, timeout=60)  
+    return "Webhook set", 200  
+  
+if __name__ == '__main__':  
     app.run(host='0.0.0.0', port=PORT)
