e29542328eed5703d912aa75977d8a90e9cb77c6
gauravbhalar95 <141895800+gauravbhalar95@users.noreply.github.com>
2025-02-23 20:51:49 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,20 +1,20 @@
 from flask import Flask, request
 from telebot import types
-from config import API_TOKEN, WEBHOOK_URL, PORT
+from config import API_TOKEN, WEBHOOK_URL
 from bot import bot
 
 app = Flask(__name__)
 
 @app.route('/' + API_TOKEN, methods=['POST'])
 def webhook():
-    bot.process_new_updates([types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "OK", 200
+    try:
+        bot.process_new_updates([types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "OK", 200
+    except Exception as e:
+        return str(e), 500
 
 @app.route('/')
 def set_webhook():
     bot.remove_webhook()
     bot.set_webhook(url=f"{WEBHOOK_URL}/{API_TOKEN}", timeout=60)
     return "Webhook set", 200
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=PORT, debug=True)  # Debug mode enabled
