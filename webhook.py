d4160abb6093f0435aa77128cf1a9ebc66411334
gauravbhalar95 <141895800+gauravbhalar95@users.noreply.github.com>
2025-02-28 08:47:16 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,7 +1,11 @@
+import os
 from flask import Flask, request
 from telebot import types
-from config import API_TOKEN, WEBHOOK_URL, PORT
+from config import API_TOKEN, WEBHOOK_URL
 from bot import bot
+
+# Fetch PORT dynamically
+PORT = int(os.environ.get("PORT", 9000))
 
 app = Flask(__name__)
 
@@ -10,11 +14,11 @@ def webhook():
     bot.process_new_updates([types.Update.de_json(request.stream.read().decode("utf-8"))])
     return "OK", 200
 
-@app.route('/')
+@app.route('/set_webhook')
 def set_webhook():
     bot.remove_webhook()
-    bot.set_webhook(url=f"{WEBHOOK_URL}/{API_TOKEN}", timeout=60)
-    return "Webhook set", 200
+    bot.set_webhook(url=f"{WEBHOOK_URL}/{API_TOKEN}")
+    return "Webhook set successfully", 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=PORT)
