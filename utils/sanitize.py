dc978b7c74351126cff5f26c3356f886f2969fab
gauravbhalar95 <141895800+gauravbhalar95@users.noreply.github.com>
2025-03-16 15:02:21 +0530

    Update sanitize.py

--- utils/sanitize.py
+++ utils/sanitize.py
@@ -10,13 +10,6 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 def sanitize_filename(filename, max_length=250):
     """
     Synchronously removes special characters from the filename and trims it to a maximum length.
-
-    Args:
-        filename (str): The original filename.
-        max_length (int): Maximum allowed length for the filename.
-
-    Returns:
-        str: The sanitized filename.
     """
     clean_name = re.sub(r'[\\/*?:"<>|]', '_', filename).strip()
     clean_name = re.sub(r'[^\x00-\x7F]+', '', clean_name)  # Remove non-ASCII characters
@@ -29,13 +22,5 @@ def sanitize_filename(filename, max_length=250):
 async def sanitize_filename_async(filename, max_length=250):
     """
     Asynchronously removes special characters from the filename and trims it to a maximum length.
-
-    Args:
-        filename (str): The original filename.
-        max_length (int): Maximum allowed length for the filename.
-
-    Returns:
-        str: The sanitized filename.
     """
-    loop = asyncio.get_running_loop()
-    return await loop.run_in_executor(None, sanitize_filename, filename, max_length)
+    return await asyncio.to_thread(sanitize_filename, filename, max_length)
