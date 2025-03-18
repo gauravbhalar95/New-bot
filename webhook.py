d7c2f6d202351fb225b3a53e52919a2fe161d880
gauravbhalar95 <141895800+gauravbhalar95@users.noreply.github.com>
2025-03-18 08:18:48 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,11 +1,11 @@
 import os
 import logging
-from flask import Flask, request, jsonify
+import asyncio
+from quart import Quart, request
 from dotenv import load_dotenv
 import telebot
 from telebot.async_telebot import AsyncTeleBot
 from config import API_TOKEN, WEBHOOK_URL, PORT
-from quart import Quart
 
 # Load environment variables
 load_dotenv()
@@ -17,19 +17,19 @@ logger = logging.getLogger(__name__)
 # Initialize bot
 bot = AsyncTeleBot(API_TOKEN, parse_mode="HTML")
 
-
+# Initialize Quart app
 app = Quart(__name__)
 
 @app.route('/' + API_TOKEN, methods=['POST'])
 async def webhook():
-    data = await request.stream.read()
-    bot.process_new_updates([telebot.types.Update.de_json(data.decode("utf-8"))])
+    data = await request.get_data()
+    await bot.process_new_updates([telebot.types.Update.de_json(data.decode("utf-8"))])
     return "OK", 200
 
 @app.route('/')
 async def set_webhook():
-    bot.remove_webhook()
-    await asyncio.to_thread(bot.set_webhook, url=WEBHOOK_URL + '/' + API_TOKEN, timeout=60)
+    await bot.remove_webhook()
+    await bot.set_webhook(url=WEBHOOK_URL + '/' + API_TOKEN, timeout=60)
     return "Webhook set", 200
 
 if __name__ == '__main__':
