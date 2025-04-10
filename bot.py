77fa1cc065f1e3d8ce9abfa35329891add5ff517
gauravbhalar95 <141895800+gauravbhalar95@users.noreply.github.com>
2025-04-11 00:42:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ bot = AsyncTeleBot(API_TOKEN, parse_mode="HTML")
 download_queue = asyncio.Queue()
 
 # Dropbox client setup
-dbx = dropbox.Dropbox(get_access_token)
+dbx = dropbox.Dropbox(access_token)
 
 # Regex patterns for different platforms
 PLATFORM_PATTERNS = {
