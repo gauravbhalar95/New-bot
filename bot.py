f6f53ad93de96d2464f735477d26f8746f862594
gauravbhalar95 <141895800+gauravbhalar95@users.noreply.github.com>
2025-09-20 14:39:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,8 +8,6 @@ import psutil
 import json
 import sys
 import time
-import Crypto
-from Crypto.Cipher import AES
 from datetime import datetime, timezone
 from mega import Mega
 from telebot.async_telebot import AsyncTeleBot
