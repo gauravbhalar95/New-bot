4a5b363329b558877dc0beb206e77c85ca08de08
gauravbhalar95 <141895800+gauravbhalar95@users.noreply.github.com>
2025-12-04 22:54:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -71,7 +71,7 @@ os.makedirs(DOWNLOAD_DIR3, exist_ok=True)
 # Cookies file for authenticated downloads
 X_FILE = "x.txt"
 YOUTUBE_FILE = "youtube_cookies.txt"
-INSTAGRAM_FILE = "www.instagram.com_cookies.txt"
+INSTAGRAM_FILE = "ig.txt"
 COOKIES_FILE = "cookies.txt"
 FACEBOOK_FILE = "facebook.txt"
 
